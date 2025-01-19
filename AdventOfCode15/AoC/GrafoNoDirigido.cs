3d778425448a06cac126a6445bbeab402fca6fc6
Luubstar <nbaronariera@gmail.com>
2025-01-19 18:50:42 +0100

    dia1 2024

--- AdventOfCode15/AoC/GrafoNoDirigido.cs
+++
@@ -1,9 +0,0 @@
-namespace commons{
-    class GrafoNoDirigo : Grafo
-    {         
-        public override void AddConnection(string o, string d, int c){
-            base.AddConnection(o,d,c);
-            base.AddConnection(d,o,c);
-        }
-    }
-}
